38ebfc8621d2bf6f21669b7d3525a247da03991f
Mayur <kapseamruta50@gmail.com>
2020-06-25 19:17:49 +0530

    From American Guy

--- src/JavaSample.java
+++ src/JavaSample.java
@@ -5,18 +5,20 @@ public class JavaSample {
 		// TODO Auto-generated method stub
 
 		JavaSample s=new JavaSample();
-		int a=8,b=9,c = 0;
+		int a=8,b=9,c = 7;
 		System.out.println("We are at main class");
 		s.addition(a,b,c);
 		s.multiplication(a, b, c);
 		s.substraction(a, b, c);
 		s.avarage(a, b, c);
+		s.percentage(a, b, c);
+		
 	
 	}
 public void addition(int a, int b,int c)
 {
 	
-	int f=a+b;
+	int f=a+b+c;
 	System.out.println(f);
 	
 	}
@@ -38,6 +40,13 @@ public void avarage(int a, int b,int c)
 {
 	
 	int f=(a+b+c)/3;
+	System.out.println(f);
+	
+	}
+public void percentage(int a, int b,int c)
+{
+	
+	int f=((a+b+c)/30)*100;
 	System.out.println(f);
 	
 	}
