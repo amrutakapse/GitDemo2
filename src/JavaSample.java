fd4ef0be85463a1bcbbff0427420e98a3c41991f
Mayur <kapseamruta50@gmail.com>
2020-06-25 17:22:53 +0530

    this is a commit from asian timezone guy

--- src/JavaSample.java
+++ src/JavaSample.java
@@ -7,13 +7,20 @@ public class JavaSample {
 		JavaSample s=new JavaSample();
 		int a=8,b=9,c = 0;
 		s.addition(a,b,c);
-		
+		s.multiplication(a, b, c);
 		
 	}
 public void addition(int a, int b,int c)
 {
 	
 	int f=a+b;
+	System.out.println(f);
+	
+	}
+public void multiplication(int a, int b,int c)
+{
+	
+	int f=a*b;
 	System.out.println(f);
 	
 	}
