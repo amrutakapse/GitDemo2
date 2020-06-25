cb4294ed7ef48b9cbb61b8aa5b39ca6f6f616a17
Mayur <kapseamruta50@gmail.com>
2020-06-25 18:58:27 +0530

    Architect asian  Development

--- src/JavaSample.java
+++ src/JavaSample.java
@@ -6,10 +6,12 @@ public class JavaSample {
 
 		JavaSample s=new JavaSample();
 		int a=8,b=9,c = 0;
+		System.out.println("We are at main class");
 		s.addition(a,b,c);
 		s.multiplication(a, b, c);
 		s.substraction(a, b, c);
-		
+		s.avarage(a, b, c);
+	
 	}
 public void addition(int a, int b,int c)
 {
@@ -29,6 +31,13 @@ public void substraction(int a, int b,int c)
 {
 	
 	int f=a-b;
+	System.out.println(f);
+	
+	}
+public void avarage(int a, int b,int c)
+{
+	
+	int f=(a+b+c)/3;
 	System.out.println(f);
 	
 	}
